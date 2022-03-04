f1043115c36e2600d4660f98e16f035c88379a12
Anton Minchonak <anton.minchonak@gmail.com>
2022-03-04 17:02:04 +0300

    Trash removed

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 var ip = require("ip");
 let addressIP = '127.0.0.1';
 if (ip.address() === "10.20.5.50") addressIP = "10.20.5.50";
+if (ip.address() === "192.168.56.1") addressIP = "192.168.0.100";
 
   async function bootstrap() {
     const app = await NestFactory.create(AppModule);
