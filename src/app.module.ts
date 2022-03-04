56dfb481f83a5f967ec03777f7f9d5f24bf71695
Anton Minchonak <anton.minchonak@gmail.com>
2022-03-04 17:04:12 +0300

    Trash removed

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { FistingController } from './fisting/fisting.controller';
 import { ProductsModule } from './products/products.module';
 import { ArticleController } from './article.controller';
 import { NotesModule } from './notes/notes.module';
