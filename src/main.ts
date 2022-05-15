c5ebecd4fde375d3f66290b76eaa37e363db5929
Anton Minchonak <anton.minchonak@gmail.com>
2022-05-15 21:00:59 +0300

    ip fix

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 var ip = require("ip");
 let addressIP = '127.0.0.1';
-if (ip.address() === "10.20.5.50") addressIP = "10.20.5.50";
-if (ip.address() === "192.168.56.1") addressIP = "192.168.0.100";
+// if (ip.address() === "192.168.56.1" || ip.address() === "26.223.200.91"  ) addressIP = "192.168.0.100";
 
   async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
     await app.listen(3000, addressIP);
+    // console.log(ip.address());
+    
   }
 bootstrap();
